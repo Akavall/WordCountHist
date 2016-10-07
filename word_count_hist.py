d7ebd404faaaf6bacc5f155dd86947b322e8be6d
Kirill <Akavall@gmail.com>
2016-10-07 11:43:02 -0700

    removed file and comment

--- word_count_hist.py
+++ word_count_hist.py
@@ -7,7 +7,6 @@ from utilities.walk_all_files import make_all_words_counter
 from utilities.graphs import histogram
 
 if __name__ == "__main__":
-    # "/home/kirill/MaanaExercise/data/"
     directory_path = sys.argv[1]
     all_words_counter = make_all_words_counter("/home/kirill/MaanaExercise/data/")
     histogram(all_words_counter.values())
