eaaac135aa0b92fb464013fa846101936f5d08ef
Kirill <Akavall@gmail.com>
2016-10-07 11:47:18 -0700

    logging error when list given to histogram is empty

--- utilities/graphs.py
+++ utilities/graphs.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import logging
 
 def histogram(my_list):
     # taking square root of number of values in list
     # a common rule of thumb method for finding number
     # of bins
+    if not my_list:
+        logging.error("list provided to histogram is empty, check that you provided the correct directory")
+        return
     n_bins = int(np.sqrt(len(my_list)))
     plt.hist(my_list, n_bins)
     plt.title("Distribution of word counts")
--- word_count_hist.py
+++ word_count_hist.py
@@ -8,7 +8,7 @@ from utilities.graphs import histogram
 
 if __name__ == "__main__":
     directory_path = sys.argv[1]
-    all_words_counter = make_all_words_counter("/home/kirill/MaanaExercise/data/")
+    all_words_counter = make_all_words_counter(directory_path)
     histogram(all_words_counter.values())
 
     
